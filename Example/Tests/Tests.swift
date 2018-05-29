7c1bc86bd8ba0395fa330d226a1ab4c2cd46cce0
Jacob Greenfield <jacob@jacobgreenfield.me>
2018-05-28 22:00:29 -0400

    Update tests and modify Travis (no bundler)

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,29 +2,6 @@ import XCTest
 import SwiftConfig
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testComputerName() {
         if let computerName = DynamicStore(name: "SwiftConfig")?.computerInfo.name as String? {
             print("Computer name: \(computerName)")
@@ -37,14 +14,12 @@ class Tests: XCTestCase {
         if let firstActive = ConfigPreferences(name: "SwiftConfig")?
             .currentNetworkSet?
             .services?
-            .filter({ $0.enabled && $0.active })
-            .first?
+            .first(where: { $0.enabled && ($0.interface?.active ?? false) })?
             .name {
             print("First active service: \(firstActive)")
             return
         }
         XCTFail()
     }
-    
 }
 
